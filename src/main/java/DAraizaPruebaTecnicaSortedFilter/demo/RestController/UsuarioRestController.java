2782403ee4772908315cc970709864f01a0f07e9
Digis 01 <digis01Sistemas@outlook.com>
2026-04-14 19:15:37 -0600

    SorterFilter

--- src/main/java/DAraizaPruebaTecnicaSortedFilter/demo/RestController/UsuarioRestController.java
+++ src/main/java/DAraizaPruebaTecnicaSortedFilter/demo/RestController/UsuarioRestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author digis
  */
 @RestController
-@RequestMapping("nuevo")
+@RequestMapping("api")
 public class UsuarioRestController {
     
     @Autowired
